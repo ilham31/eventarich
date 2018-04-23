a9f31474d7a5f3972110eb2fb7104b156aa5ee78
firgiawansaktyo <firgiawansaktyo@gmail.com>
2018-04-23 16:31:54 +0700

    pull wae

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {FilterPage} from '../pages/filter/filter';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = FilterPage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
